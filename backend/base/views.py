f913e30924aafc5dc9cf694e7a6ee937b0fef164
NithinGoud2605 <sainithingoud007@gmail.com>
2024-11-06 18:31:06 -0500

    add

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,31 +1,55 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-
-from .serializers import ProductSerializer
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from .models import Product
-from .products import products
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+        
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
-
     routes = [
-        '/api/products/'
-        '/api/products/create/'
-
-        '/api/products/upload/'
-
-        '/api/products/<id>/reviews/'
-
-        '/api/products/top/'
-        '/api/products/<id>/'
-
-        '/api/products/delete/<id>/'
-        '/api/products/<update>/<id>/'
+        '/api/products/',
+        '/api/products/create/',
+        '/api/products/upload/',
+        '/api/products/<id>/reviews/',
+        '/api/products/top/',
+        '/api/products/<id>/',
+        '/api/products/delete/<id>/',
+        '/api/products/update/<id>/',
+        '/api/users/profile/',
+        '/api/users/',  # Added for clarity
     ]
-
     return Response(routes)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
@@ -33,10 +57,12 @@ def getProducts(request):
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
-
 @api_view(['GET'])
 def getProduct(request, pk):
+    try:
+        product = Product.objects.get(_id=pk)
+    except Product.DoesNotExist:
+        return Response({'detail': 'Product not found'}, status=404)
 
-    product=Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
