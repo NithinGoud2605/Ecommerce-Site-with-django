9f4cee218a1824c3f49b61248e549313df56dec0
NithinGoud2605 <sainithingoud007@gmail.com>
2024-11-11 23:51:11 -0500

    add

--- backend/settings.py
+++ backend/settings.py
@@ -110,8 +110,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = 'https://handmadehub-bucket.s3.amazonaws.com/'  # Production media URL (S3)
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    BASE_DIR / 'frontend/dist/assets'
+    BASE_DIR / 'frontend' / 'dist',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Local storage for static files
