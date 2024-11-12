f45558e40cbdf684828a0b0042e5d4ed330759b5
NithinGoud2605 <sainithingoud007@gmail.com>
2024-11-12 01:53:56 -0500

    deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/static/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',  // Replace with your backend server URL
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+  base: '/static/',  // Align with Django's STATIC_URL
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 });
